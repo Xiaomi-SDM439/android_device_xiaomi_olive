fa017c3e5737e666cd1cd80847c46e97b718551f
Louis Popi <theh2o64@gmail.com>
2019-12-07 21:47:56 +0100

    olive: Use olive-perf_defconfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -44,7 +44,7 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_serial_dm,0x78B0000 firmware_class.path=/vendor/firmware_mnt/image androidboot.usbconfigfs=true loop.max_part=7
-TARGET_KERNEL_CONFIG := olive_defconfig
+TARGET_KERNEL_CONFIG := olive-perf_defconfig
 
 # DRM
 TARGET_ENABLE_MEDIADRM_64 := true
