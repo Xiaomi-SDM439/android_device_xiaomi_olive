7247e969fa4375de13afa80a30b24fc21517e8c9
EngMoPro <fightstart20000@yahoo.com>
2020-07-17 17:31:03 +0200

    olive:delete hidl already added the to common tree

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -40,9 +40,7 @@ TARGET_OTA_ASSERT_DEVICE := olive
 # Security patch level
 VENDOR_SECURITY_PATCH := 2020-01-01
 
-# HIDL
-DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(DEVICE_PATH)/framework_compatibility_matrix.xml
+
 
 # Props
 TARGET_SYSTEM_PROP += device/xiaomi/olive/system.prop
