b305d33d329701dd5436671734a021c61f725a77
Louis Popi <theh2o64@gmail.com>
2020-07-25 05:53:50 +0200

    Revert "olive:delete hidl already added the to common tree"
    This reverts commit 7247e969fa4375de13afa80a30b24fc21517e8c9.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -40,6 +40,10 @@ TARGET_OTA_ASSERT_DEVICE := olive
 # Security patch level
 VENDOR_SECURITY_PATCH := 2020-01-01
 
+# HIDL
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(DEVICE_PATH)/framework_compatibility_matrix.xml
+
 # Props
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 
